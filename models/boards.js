7416fa3ffccde9eefdc66df050d0efa7756ae54a
Daniel <daniel.reguero@hotmail.com>
2016-10-21 20:49:53 -0400

    Added boards models.
    Need two more.

--- models/boards.js
+++ models/boards.js
@@ -1,1 +1,27 @@
-var sequelize = require("./config/index").getSequelize();
+var sequelize = require("../config/index").getSequelize();
+var Sequelize = require('sequelize');
+var Boards = sequelize.define('boards', {
+    name: {
+        type: Sequelize.STRING(150),
+        field: 'name',
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING(150),
+        field: 'slug',
+        allowNull: false
+    }
+});
+//Default, create pol
+Boards.findOrCreate({
+    where:{slug: "pol"}, 
+    defaults: {name: "Politically Incorrect", slug: "pol"}
+}).then(function(board, created){
+    if(created){
+        console.log("pol board not found, it was created");
+    }
+}).catch(function(err){
+    console.log("DB Error: " + err);
+});
+Boards.sync();
+module.exports = Boards;
