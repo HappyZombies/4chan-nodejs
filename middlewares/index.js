984db64ae80a8697bf97314803d01f09f4f8b86c
Daniel <daniel.reguero@hotmail.com>
2016-10-21 20:50:01 -0400

    Experimenting with middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+var Boards = require("../models/boards");
+module.exports = {
+    // setLocals: function(req, res, next) {
+    //     Boards.findAll().then(function(boards){
+    //         res.locals.boards = boards;
+    //     }).catch(function(err){
+    //         res.locals.boards = "";
+    //     });
+    //     next();
+    // }
+}
