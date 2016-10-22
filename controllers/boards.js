2ed97bdcabc71ae537572e2b92c5e96b40469089
Daniel <daniel.reguero@hotmail.com>
2016-10-21 20:50:14 -0400

    Added controllers for boards and home page.

---
+++ controllers/boards.js
@@ -0,0 +1,24 @@
+/**
+ * Controllers (or routes) for our boards!
+ */
+
+module.exports = function(app, Boards){
+
+    app.get('/board/:slug', function(req, res){
+        Boards.findOne({
+            where: {
+                slug: req.params.slug
+            }
+        }).then(function(boards){
+            console.dir(boards);
+            if(boards == null){
+                res.status(400).send({ error: 'Not Found!' });
+            }else{
+                res.render('boards', {boards: boards});
+            }
+        }).catch(function(err){
+            res.status(500).send({ error: 'Something went wrong!' });
+        })
+    });
+
+}
