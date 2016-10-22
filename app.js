c552f95a5a4b2b3df1672afc09d69a5ff09d0ca4
Daniel <daniel.reguero@hotmail.com>
2016-10-21 20:50:29 -0400

    Required in controllers made.

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 require('dotenv').config();
-
+var Boards = require("./models/boards");
+var middlewares = require("./middlewares");
 //'Custom' modules/variables.
 var port = process.env.PORT || 3000;
-var indexController = require("./controllers");
+var indexController = require("./controllers")
+var boardController = require("./controllers/boards");
 
 // Begin Middleware
 app.use('/assets', express.static(__dirname + "/public"));
@@ -18,12 +20,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+//app.use(middlewares.setLocals); not yet
 app.set('view engine', 'ejs');
 
 
 //Call in our controllers/routes
-indexController(app);
-
+indexController(app, Boards);
+boardController(app, Boards);
 //Listen on port specified.
 app.listen(port, function(){
     console.log("Website is running on http://" + process.env.HOST + ":" + port);
