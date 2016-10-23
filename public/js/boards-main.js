71ab0b4441c1567a2deb3d1c193140bf33a41b84
Daniel <daniel.reguero@hotmail.com>
2016-10-23 19:35:52 -0400

    Began adding form to create a new thread.
    I probably will push out styling and begin focusing on the backend. The
    front-end is killing me atm.

--- public/js/boards-main.js
+++ public/js/boards-main.js
@@ -1,7 +1,10 @@
 /**
  * Created by Daniel Reguero on 10/23/2016.
  */
+var toggle_form_button = document.getElementById("post-form-toggle");
+var post_thread_form = document.getElementById("post-thread-form")
 function randomBanner(){
+    //lol lazy
     var images = new Array("1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif");
     var imageNum = Math.floor(Math.random() * images.length);
     var baseUrl = document.location.origin;
@@ -9,3 +12,9 @@ function randomBanner(){
 }
 
 window.onload = randomBanner();
+
+toggle_form_button.addEventListener('click', function(e){
+    e.preventDefault();
+    post_thread_form.style.display = "block";
+    toggle_form_button.style.display = "none"; //why does 4chan hide their toggle ?
+}, false);
