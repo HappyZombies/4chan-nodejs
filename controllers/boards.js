2fd25b9f673404398e9635eb53a2d490098a2009
Daniel <daniel.reguero@hotmail.com>
2017-01-08 14:35:14 -0500

    Added reply method & added "bumping" mechanism.

--- controllers/boards.js
+++ controllers/boards.js
@@ -19,7 +19,7 @@ module.exports = function(app, Boards, Threads, Comments){
                     where: { board_id: board.id },
                     limit: 5,
                     order: "createdAt DESC",
-                    include: [ {model: Comments, as: "comments", limit: 5, order: "createdAt ASC"} ]
+                    include: [ {model: Comments, as: "comments", limit: 5, order: "createdAt DESC"} ]
                 }).then(function(threads){
                     res.render('boards', {board: board, threads: threads});
                 }).catch(function(err){
@@ -57,9 +57,7 @@ module.exports = function(app, Boards, Threads, Comments){
         });
     });
 
-
     //Post methods.
-    
     app.post('/board/:slug/thread', function(req, res){
         Boards.findOne({
             where: {
@@ -76,7 +74,6 @@ module.exports = function(app, Boards, Threads, Comments){
                     comment: req.body.comment,
                     file: req.body.upfile
                 }).then(function(thread){
-                    console.dir("Create??");
                     res.redirect('/board/'+req.params.slug+'/thread/'+thread.id);
                 }).catch(function(err){
                     console.dir(err);
@@ -87,6 +84,44 @@ module.exports = function(app, Boards, Threads, Comments){
             console.dir(err);
             res.status(500).send({ error: 'Something went wrong when finding...' });
         })
+    });
+
+    app.post('/board/:slug/thread/:thread_id', function(req, res){
+        //Let's still check if we are in the proper board 
+        Boards.findOne({
+            where: {
+                slug: req.params.slug
+            }
+        }).then(function(board){
+            if(board == null){
+                res.status(400).send({ error: 'Board Not Found!' });
+            }else{
+                 if(!isNaN(req.params.thread_id)){
+                    //threads are numbers.
+                    Threads.findById(req.params.thread_id).then(function(thread){
+                        if(thread === null){
+                            res.status(400).send({ error: 'Thread Not Found!' });                         
+                        }else{
+                            //Create a comment.
+                            Comments.create({
+                                    threadId: req.params.thread_id,
+                                    author: req.body.name,
+                                    comment: req.body.comment,
+                                    file: req.body.upfile
+                            }).then(function(comment){
+                                //After we create the new comment. Update the thread with the new create date FROM the comment.
+                                Threads.update({createdAt: comment.createdAt}, {where:{id: req.params.thread_id}});
+                                res.redirect(req.originalUrl);                                
+                            });
+                        }
+                    });
+                }else{
+                    res.status(400).send({ error: 'Thread Not Found!' });
+                }
+            }
+        }).catch(function(err){
+            res.status(500).send({ error: 'Something went wrong!' });
+        });
     });
 
 };
