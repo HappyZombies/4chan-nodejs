9057ef4dc4b128315b2e0af4811ff7451c9b1812
Daniel <daniel.reguero@hotmail.com>
2017-01-08 20:54:52 -0500

    Added pagination code

--- controllers/boards.js
+++ controllers/boards.js
@@ -5,8 +5,7 @@
 module.exports = function(app, Boards, Threads, Comments){
 
     //view board with all threads.
-    app.get('/board/:slug', function(req, res){
-
+    app.get('/board/:slug/:page?', function(req, res){
         Boards.findOne({
             where: {
                 slug: req.params.slug
@@ -15,20 +14,38 @@ module.exports = function(app, Boards, Threads, Comments){
             if(board == null){
                 res.status(400).send({ error: 'Board Not Found!' });
             }else{
-                Threads.findAll({
+                //Determine what we want to do... 
+                //FIXME: This could all be in it's own method.
+                var threadsPerpage = 5; //4chan has 20. Leave 5 for now.
+                var pageRequested = req.params.page ? (parseInt(req.params.page, 10) || 0 )-1 : 0;
+                var numOfPages = 10; //total of 10 pages.
+                var offSet = pageRequested * threadsPerpage;
+                var threadConfig = {
                     where: { board_id: board.id },
-                    limit: 5,
+                    offset: offSet,
+                    limit: threadsPerpage,
                     order: "createdAt DESC",
                     include: [ {model: Comments, as: "comments", limit: 5, order: "createdAt DESC"} ]
-                }).then(function(threads){
-                    res.render('boards', {board: board, threads: threads});
-                }).catch(function(err){
-                    res.status(500).send({ error: 'Something went wrong!' });
-                });
+                };
+                //Are they over the page limit allowed ?, is it 
+                if((!isNaN(req.params.page)  || req.params.page === undefined) &&(pageRequested < numOfPages)){
+                     Threads.findAll(threadConfig).then(function(threads){
+                         threads.pagination = {
+                             nextPage: pageRequested < numOfPages - 2 ? pageRequested + 2 : undefined
+                         }
+                        res.render('boards', {board: board, threads: threads});
+                    }).catch(function(err){
+                        res.status(500).send({ error: 'Something went wrong!' });
+                    });
+                }else{
+                    //They are over (aka, on page 11. No such thing exists).
+                    res.status(500).send({ error: 'There are only 10 pages avialable and/or you entered something that wasnt a number' });                    
+                }
+               
             }
         }).catch(function(err){
             res.status(500).send({ error: 'Something went wrong!' });
-        })
+        });
     });
 
     //view individual thread
